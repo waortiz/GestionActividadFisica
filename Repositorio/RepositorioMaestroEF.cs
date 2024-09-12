aae4802ff589775070260b453de2eab5ada1d738
William <waortiz@hotmail.com>
2024-09-11 20:49:58 -0500

    Creación Repositorio Maestro para usar Entity Framework

---
+++ Repositorio/RepositorioMaestroEF.cs
@@ -0,0 +1,23 @@
+﻿using Entidades;
+
+namespace Repositorio
+{
+    public class RepositorioMaestroEF : IRepositorioMaestro
+    {
+        public List<Ciudad> ObtenerCiudades()
+        {
+            var ciudades = new List<Ciudad>();
+                        
+
+            return ciudades;
+        }
+
+        public List<TipoDocumento> ObtenerTiposDocumento()
+        {
+            var tiposDocumento = new List<TipoDocumento>();
+
+            
+            return tiposDocumento;
+        }
+    }
+}
